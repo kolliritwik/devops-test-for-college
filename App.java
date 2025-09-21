83197da52c9524af804a79fd1415a3e418bc74b9
Rishi <k.krishnarishi@icloud.com>
2025-09-22 00:45:11 +0530

    webhook second test

--- App.java
+++ App.java
@@ -2,10 +2,10 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins CI! 123");
+        System.out.println("Hello, Jenkins CI! 1234");
     }
 
     public String getMessage() {
-        return "Hello, Jenkins CI! 123";
+        return "Hello, Jenkins CI! 1234";
     }
 }
